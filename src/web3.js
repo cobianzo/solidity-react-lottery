3af0299be54e415962958960154cf77c7a6f86aa
Cobianzo <cobianzo@gmail.com>
2022-04-29 19:46:44 +0200

    Update the code from the course in case I want to compare

--- src/web3.js
+++ src/web3.js
@@ -1,12 +1,13 @@
-// config file to set up erb3 with the Metamask provider. 
+// config file to set up web with the Metamask provider.
 // That will connect us directly to the ethereum network, in this case Rinkeby, which should be set in Test mode.
 
+// we wamt to use our web3 library, but metamask provider.
 import Web3 from "web3";
-     
+
 // this assumens that Metamask chrome extension is installed.
 //  it will propmpt metamask access, you will have to accepts it.
 window.ethereum.request({ method: "eth_requestAccounts" });
- 
+
 const web3 = new Web3(window.ethereum);
- 
+
 export default web3;
